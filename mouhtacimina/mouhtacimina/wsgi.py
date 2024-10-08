0c8ff86f864179d12a5bdecee215ed36e25d3504
OusseynouNdour <ndour.ousseynou@ugb.edu.sn>
2024-10-08 00:17:21 +0000

    Premier commit sur le projet mouhtacimina

--- mouhtacimina/mouhtacimina/wsgi.py
+++ mouhtacimina/mouhtacimina/wsgi.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 WSGI config for mouhtacimina project.
 
@@ -14,3 +15,21 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mouhtacimina.settings')
 
 application = get_wsgi_application()
+=======
+"""
+WSGI config for mouhtacimina project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mouhtacimina.settings')
+
+application = get_wsgi_application()
+>>>>>>> 35cf3e7 (Premier commit sur le projet mouhtacimina)
