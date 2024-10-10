c171e1c7574fc79eb86856d7298c287569a0fca5
OusseynouNdour <ndour.ousseynou@ugb.edu.sn>
2024-10-10 23:35:24 +0000

    apres encore des bugs

--- mouhtacimina/profils/views.py
+++ mouhtacimina/profils/views.py
@@ -1,4 +1,11 @@
 # profils/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Member
+from .serializers import MemberSerializer
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
 
