0c6e4791bec7eb85e772bce65bf79a6647ec10db
OusseynouNdour <ndour.ousseynou@ugb.edu.sn>
2024-10-10 23:29:08 +0000

    apres bugs

--- mouhtacimina/profils/views.py
+++ mouhtacimina/profils/views.py
@@ -1,14 +1,4 @@
 # profils/views.py
 
-<<<<<<< HEAD
 # Create your views here.
 
-=======
-from rest_framework import viewsets
-from .models import Member
-from .serializers import MemberSerializer
-
-class MemberViewSet(viewsets.ModelViewSet):
-    queryset = Member.objects.all()
-    serializer_class = MemberSerializer
->>>>>>> 2cc3dc8 (deuxieme commit sur le projet django)
