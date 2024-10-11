a30c49507cfeb1a0ddd3cbf5c028477182c0fedb
OusseynouNdour <ndour.ousseynou@ugb.edu.sn>
2024-10-11 00:02:34 +0000

    apres encore et encore des bugs bugs

--- mouhtacimina/mouhtacimina/settings.py
+++ mouhtacimina/mouhtacimina/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mouhtacima',
+        'NAME': 'mouhtacimina',
         'USER': 'postgres',
         'PASSWORD': 'Alkhalifa@7',
         'HOST': 'localhost',  # Ou l'adresse IP de ton serveur PostgreSQL
