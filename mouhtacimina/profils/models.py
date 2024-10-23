01967c199bf1f84cb9629f4726747976492fd1dc
OusseynouNdour <ndour.ousseynou@ugb.edu.sn>
2024-10-23 18:18:38 +0000

    ajout de nouveau champ à Member

--- mouhtacimina/profils/models.py
+++ mouhtacimina/profils/models.py
@@ -15,11 +15,31 @@ class MemberType(models.Model):
         return self.type_name
 
 class Member(models.Model):
+    class Ufr(models.TextChoices):
+        SAT = 'SAT',
+        SEG = 'SEG',
+        IPSL = 'IPSL',
+        Sante = '2S',
+        AGRO = '2SATA',
+        DROIT = 'SJP'
+
+    class Martial_status(models.TextChoices):
+        Marie = 'Marié',
+        Celib = 'Célibataire'
+
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     section = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True)
     member_type = models.ForeignKey(MemberType, on_delete=models.SET_NULL, null=True)
     date_joined = models.DateField(auto_now_add=True)
+    address = models.fields.CharField(max_length=100)
+    study_level = models.fields.CharField(max_length=20)
+    ufr = models.fields.CharField(choices=Ufr.choices, max_length=5)
+    speciality = models.fields.CharField(max_length=20)
+    promo = models.fields.CharField(max_length=3)
+    statut = models.fields.CharField(max_length=20)
+    activity = models.fields.CharField(max_length=20)
+    Marital_status = models.fields.CharField(choices=Martial_status.choices,max_length=12)
 
     def __str__(self):
         return self.username
